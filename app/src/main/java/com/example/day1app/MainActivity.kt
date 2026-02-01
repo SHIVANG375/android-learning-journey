5428ace44d1f70c2adf981b54b29516172fdcde9
Shivang Chauhan <chauhanshivang975@gmail.com>
2026-02-01 16:13:11 +0530

    simple 2 screen app with input and data pass through intent

--- app/src/main/java/com/example/day1app/MainActivity.kt
+++ app/src/main/java/com/example/day1app/MainActivity.kt
@@ -5,6 +5,8 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.EditText
+import android.content.Intent
+import android.util.Log
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,8 +17,27 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.btnClick)
 
         button.setOnClickListener {
-            val name = input.text.toString()
-            text1.text = "Hello, $name"
+            val name=input.text.toString()
+            val intent = Intent(this,SecondActivity::class.java)
+            intent.putExtra("USERNAME",name)
+            startActivity(intent)
         }
     }
+    override fun onStart() {
+        super.onStart()
+        Log.d("Lifecycle", "onStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.d("LifeCycle","onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("LifeCycle", "onPause")
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.d("LifeCycle","onStop")
+    }
+
 }
---
+++ app/src/main/java/com/example/day1app/SecondActivity.kt
@@ -0,0 +1,19 @@
+package com.example.day1app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        val tv=findViewById<TextView>(R.id.tvSecond)
+        val name=intent.getStringExtra("USERNAME")
+        tv.text="HELLO, $name"
+
+    }
+}
